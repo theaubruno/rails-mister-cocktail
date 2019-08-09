2329e3dd7192b678d7f84c8d0910ec1bf5d1a78e
Théau Bruno <bn.theau@gmail.com>
2019-08-09 12:34:18 +0200

    DB & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "wine")
-Ingredient.create(name: "maracuja")
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint Leaves")
+Ingredient.create(name: "Wine")
+Ingredient.create(name: "Maracuja")
 Ingredient.create(name: "Agrumes")
-
-Cocktail.create(name: "Caipirina")
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Sangria")
-
-Doses.create(description: "3", cocktail_id: 3, ingredient_id: 1)
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Whisky")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Coca")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Strawberries")
+Ingredient.create(name: "Cherry")
+Ingredient.create(name: "Letchi")
